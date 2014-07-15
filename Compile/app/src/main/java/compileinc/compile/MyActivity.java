a4d29540026a68888e0edd2b2c25f51f5cfa7caa
Jonathan Caicedo <jbirdcaicedo@yahoo.com>
2014-07-15 10:08:34 -0400

    FrontEnd development
    Added add_contact action and replaced overflow icon with add icon

--- Compile/app/src/main/java/compileinc/compile/MyActivity.java
+++ Compile/app/src/main/java/compileinc/compile/MyActivity.java
@@ -18,7 +18,7 @@ public class MyActivity extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.my, menu);
+        getMenuInflater().inflate(R.menu.add_contact, menu);
         return true;
     }
 
@@ -28,7 +28,7 @@ public class MyActivity extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_add_contact) {
             return true;
         }
         return super.onOptionsItemSelected(item);
