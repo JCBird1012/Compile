c8624b3d6d5d08a513edb1e7f2fa6c2e7b74454c
John Gallagher <johnjon8@gmail.com>
2014-07-15 13:55:20 -0400

    java etiquette improvement

---
+++ Compile/app/src/main/java/compile_inc/compile/Contact.java
@@ -0,0 +1,93 @@
+package compile_inc.compile;
+
+/**
+ * Created by DUCA on 7/15/2014.
+ */
+public class Contact{
+    String firstName;
+    String lastName;
+    String email;
+    String address;
+    String phone_num;
+    int id;
+
+    public Contact(){
+        firstName = "";
+        lastName = "";
+        email = "";
+        address = "";
+        phone_num = "";
+    }
+    //constructor
+    public Contact(String startFirstName, String startLastName, String startEmail, String startAddress, String startPhone_num){
+        this.firstName = startFirstName;
+        this.lastName = startLastName;
+        this.email = startEmail;
+        this.address = startAddress;
+        this.phone_num = startPhone_num;
+    }
+    //constructor with id value
+    public Contact(int _id, String startFirstName, String startLastName, String startEmail, String startAddress, String startPhone_num){
+        this.firstName = startFirstName;
+        this.lastName = startLastName;
+        this.email = startEmail;
+        this.address = startAddress;
+        this.phone_num = startPhone_num;
+        this.id = _id;
+    }
+
+    //the next bunch of functions will be assigners/mutators
+    //sets id
+    public void setID(int _id) {
+        this.id = _id;
+    }
+    //sets the first name
+    public void setFirstName(String startFirstName) {
+        this.firstName = startFirstName;
+    }
+    //sets the last name
+    public void setLastName(String startLastName) {
+        this.lastName = startLastName;
+    }
+    //sets the email
+    public void setEmail(String startEmail) {
+        this.email = startEmail;
+    }
+    //sets the address
+    public void setAddress(String startAddress) {
+        this.address = startAddress;
+    }
+    //sets the phone number
+    public void setPhone_num(String startPhone_num) {
+        this.phone_num = startPhone_num;
+    }
+
+    //the next set of functions will be accessors
+    //gets thi ID of the contact
+    public int getId() {
+        return this.id;
+    }
+    //gets the first name
+    public String getFirstName() {
+        return this.firstName;
+    }
+    //gets last name
+    public String getLastName() {
+        return this.lastName;
+    }
+    //gets email
+    public String getEmail() {
+        return this.email;
+    }
+    //gets address
+    public String getAddress() {
+        return this.address;
+    }
+    //gets phone number
+    public String getPhone_num() {
+        return this.phone_num;
+    }
+
+
+
+}
