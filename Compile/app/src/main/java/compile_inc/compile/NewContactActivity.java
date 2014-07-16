3f72cb8c92f9c1cac76b03a3b535c8deaf59185b
Jonathan Caicedo <jbirdcaicedo@yahoo.com>
2014-07-16 14:04:57 -0400

    UI Change

--- Compile/app/src/main/java/compile_inc/compile/NewContactActivity.java
+++ Compile/app/src/main/java/compile_inc/compile/NewContactActivity.java
@@ -19,7 +19,7 @@ public class NewContactActivity extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.add_contact, menu);
+        getMenuInflater().inflate(R.menu.action_add_contact, menu);
         return true;
     }
 
