dd76029b0a637621cca465a3110b536cf7af2bc9
Jonathan Caicedo <jbirdcaicedo@yahoo.com>
2014-07-16 15:36:24 -0400

    Fixed other issues conflict caused

--- Compile/app/src/main/java/compile_inc/compile/MainActivity.java
+++ Compile/app/src/main/java/compile_inc/compile/MainActivity.java
@@ -128,7 +128,8 @@ public class MainActivity extends Activity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_add_contact) {
-            
+            Intent intent = new Intent(this, NewContactActivity.class);
+            startActivity(intent);
         }
         return super.onOptionsItemSelected(item);
 
