7d464f7d99e4d1cbb74fac6bfbe9a86cdc868e73
John Gallagher <johnjon8@gmail.com>
2014-07-16 15:26:40 -0400

    Testing features and Backend Contact Input

--- Compile/app/src/main/java/compile_inc/compile/NewContactActivity.java
+++ Compile/app/src/main/java/compile_inc/compile/NewContactActivity.java
@@ -3,8 +3,11 @@ package compile_inc.compile;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+
 import compile_inc.compile.R;
 
 public class NewContactActivity extends Activity {
@@ -30,6 +33,15 @@ public class NewContactActivity extends Activity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_add_contact) {
+            Log.d("Contact_add:  ", "Adding a Contact");
+            EditText text = (EditText)findViewById(R.id.new_contactFirstName);
+            String _firstName = text.getText().toString();
+            text = (EditText)findViewById(R.id.new_contactLastName);
+            String _lastName = text.getText().toString();
+            text = (EditText)findViewById(R.id.new_contactEmail);
+            String _email = text.getText().toString();
+            Contact newContact = new Contact(_firstName, _lastName, _email);
+            MainActivity.db.dbAddContact(newContact);
             return true;
         }
         return super.onOptionsItemSelected(item);
