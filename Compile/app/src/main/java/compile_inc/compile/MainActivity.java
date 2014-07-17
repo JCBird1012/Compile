5a4a3e16293ddbb3c34df952ee2934030c067e09
Jonathan Caicedo <jbirdcaicedo@yahoo.com>
2014-07-17 18:05:53 -0400

    Added Delete Button and UI changes

--- Compile/app/src/main/java/compile_inc/compile/MainActivity.java
+++ Compile/app/src/main/java/compile_inc/compile/MainActivity.java
@@ -120,6 +120,7 @@ public class MainActivity extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.delete, menu);
         getMenuInflater().inflate(R.menu.settings, menu);
         getMenuInflater().inflate(R.menu.add_contact, menu);
         return true;
