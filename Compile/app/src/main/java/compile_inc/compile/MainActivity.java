747bd45cb0446ee45d545daabc84a08d3dc29f2c
Jonathan Caicedo <jbirdcaicedo@yahoo.com>
2014-07-22 15:49:19 -0400

    COMMIT OF FACEBOOK

--- Compile/app/src/main/java/compile_inc/compile/MainActivity.java
+++ Compile/app/src/main/java/compile_inc/compile/MainActivity.java
@@ -81,6 +81,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onResume() {
         super.onResume();
+        welcomeScreen();
 
     }
     public boolean contactHasEmail(Contact contact) {
