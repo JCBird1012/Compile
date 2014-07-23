c754793e5595428b395a05e6d0a218f8f2e34985
Jonathan Caicedo <jbirdcaicedo@yahoo.com>
2014-07-22 21:34:36 -0400

    FIXED A BUG THAT WAS BUGGING ME (no pun intended)

--- Compile/app/src/main/java/compile_inc/compile/SettingsActivity.java
+++ Compile/app/src/main/java/compile_inc/compile/SettingsActivity.java
@@ -29,6 +29,7 @@ public class SettingsActivity extends FragmentActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTitle("Settings");
         setContentView(R.layout.activity_settings);
 
         if (savedInstanceState == null) {
